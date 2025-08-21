e343d457a6fe7c54d8f9e406a138f2aaa9874869
Parthip <projectcs34@gmail.com>
2025-08-21 23:49:02 +0530

    Add layout to project and todoContainer

--- src/pageload.js
+++ src/pageload.js
@@ -1,4 +1,15 @@
+import todo from './todo.js'
+
 export default function pageLoad() {
 
+    const container = document.querySelector('.todo-list');
+
+    const header = document.createElement('div');
+    header.classList.add('todo-header');
+    const title = document.createElement('div');
+
+    title.textContent = "MyProject";
+    header.append(title);
+    container.append(header);
     
 }
