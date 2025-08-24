4729e34ee9ccea56bceebe01d7d6c96888e9c173
Parthip <projectcs34@gmail.com>
2025-08-24 16:33:53 +0530

    Add new todo functionality

--- src/addTodo.js
+++ src/addTodo.js
@@ -1,13 +1,43 @@
+import Todo from "./todo";
+import displayTodos from "./displayTodos";
 
 export default function addTodo(){
     const container = document.querySelector('.todo-list')
-  const addTodoBtn = document.querySelector('.add-todo-btn');
 
     const formContainer = document.querySelector('#form-container');
+    const form = document.querySelector('#newTodoForm');
+
     if (!formContainer.classList.contains('hidden')) return;
 
     formContainer.classList.remove('hidden');
 
+    form.addEventListener('submit' ,(e) => {
+      e.preventDefault();
+
+      const newTodo = new Todo(
+          document.querySelector('#title').value,
+          document.querySelector('#desc').value,
+          document.querySelector('#dueDate').value,
+          document.querySelector('#check').checked,
+          document.querySelector('#priority').value
+      );
+
+      newTodo.id = crypto.randomUUID();
+
+
+
+    addTodoList(newTodo);
+
     
 
+        })
 }
+
+function addTodoList(todo){
+  let todos = JSON.parse(localStorage.getItem('todos')) || [];
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+  displayTodos();
+}
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./style.css"
 import pageLoad from "./pageload.js"
 
 import addTodo from "./addTodo.js";
+import displayTodos from "./displayTodos";
 const addTodoBtn = document.querySelector('.add-todo-btn');
 
 
@@ -14,6 +15,14 @@ addTodoBtn.addEventListener('click',() => {
     }else{
         addTodoBtn.classList.remove('active');
         const formContainer = document.querySelector('#form-container');
+        const form = formContainer.querySelector('form'); 
+        const textarea = form.querySelector('textarea');
+
         formContainer.classList.add('hidden');
+        form.reset(); 
+                textarea.style.width = '';  // reset size
+        textarea.style.height = '';
+
     }
+    displayTodos();
 })
